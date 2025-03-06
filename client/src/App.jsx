bb58b889b18f32cfb2b3da9f704888dbec4371c1
rapPayne <bob@creator.net>
2025-03-06 15:15:07 -0600

    Added picking logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,28 +2,59 @@ import { useState } from 'react'
 import { PeopleList } from './PeopleList';
 
 function App() {
-  const [people, setPeople] = useState([]);
+  // const [people, setPeople] = useState([]);
+  const [pickedPeople, setPickedPeople] = useState([]);
+  const [unpickedPeople, setUnpickedPeople] = useState([]);
+  const [pickedPerson, setPickedPerson] = useState(null);
+  console.log(unpickedPeople)
+  // When component loads first time, get all the people
+  // When component loads first time, set pickedPeople to all the people
+  // When pickedPerson changes, remove them from unPickedPeople.
+  // When pickedPerson changes, add them to 
   return (
     <>
-      <h1>People Picker</h1>
-      <img style={styles.logo} src="https://play-lh.googleusercontent.com/2A-bF1LdqB8TKZLSfC8tm2_QX47Bp_3RIdt3GqWaTXaaV673Uvt8Jf-s_2cW6mzeJQ=w480-h960-rw" alt="Logo" />
-      <button onClick={() => fetchAllPeople()}>Reset</button>
-      <button>Pick victim</button>
-      <h2>Picked person</h2>
-      <div>Picked person will go here eventually</div>
-      <h2>Unpicked people</h2>
-      <div>unpicked people will go here eventually</div>
-      <PeopleList listOPeople={people} />
-      <h2>Already picked people</h2>
-      <div>Picked people will go here eventually</div>
+      <header>
+        <h1>People Picker</h1>
+        <img style={styles.logo} src="https://play-lh.googleusercontent.com/2A-bF1LdqB8TKZLSfC8tm2_QX47Bp_3RIdt3GqWaTXaaV673Uvt8Jf-s_2cW6mzeJQ=w480-h960-rw" alt="Logo" />
+      </header>
+      <main>
+        <div className='topDiv'>
+          <section>
+            <button onClick={() => fetchAllPeople()}>Reset</button>
+            <button onClick={() => pickAPerson()}>Pick victim</button>
+          </section>
+          {pickedPerson && <section>
+            <h2>Picked person</h2>
+            <div>{pickedPerson?.firstName}</div>
+          </section>}
+        </div>
+        <section>
+          <h2>Unpicked people</h2>
+          {unpickedPeople.length > 0 ?
+            <PeopleList listOPeople={unpickedPeople} /> :
+            <p>All people have been picked.</p>}
+        </section>
+        <section>
+          <h2>Already picked people</h2>
+          <p>Picked people will go here eventually</p>
+        </section>
+      </main>
+      <footer>
+        <p>Copyright &copy; The best EDP cohort, 2025. All rights reserved.</p>
+      </footer>
     </>
   )
 
   async function fetchAllPeople() {
-    let people = await fetch("http://localhost:4000/api/people")
+    let ppl = await fetch("http://localhost:4000/api/people")
       .then(res => res.json())
-    setPeople(people)
-    console.log(people)
+    setUnpickedPeople(ppl)
+    console.log(ppl)
+  }
+
+  function pickAPerson() {
+    const pickedPerson = unpickedPeople[Math.floor(Math.random() * unpickedPeople.length)];
+    setPickedPerson(pickedPerson);
   }
 }
 
--- client/src/PeopleList.jsx
+++ client/src/PeopleList.jsx
@@ -4,7 +4,6 @@ export function PeopleList(props) {
   const people = props.listOPeople;
   return (
     <>
-      <h1>People list</h1>
       <section style={{ display: "flex", flexWrap: "wrap" }}>
         {people.map(p => <PersonCard key={p.id} person={p} />)}
       </section>
